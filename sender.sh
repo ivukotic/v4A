638beab8d056df95e7a8dd099e4749b6186094d8
Ilija Vukotic <ivukotic@cern.ch>
2022-07-13 12:15:51 -0500

    update sender

--- sender.sh
+++ sender.sh
@@ -2,7 +2,7 @@
 
 data=$(varnishstat -j)
 
-jsn=$(echo $data | jq --arg INST "$INSTANCE" '. += { instance: $INST }')
+jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
 
 curl --request POST -L -k \
   --url http://varnish.atlas-ml.org:80/ \
