e9ca2b5434353e463721139afecf25b249cbb80d
Ilija Vukotic <ivukotic@cern.ch>
2022-07-13 11:58:10 -0500

    added instance

--- sender.sh
+++ sender.sh
@@ -2,9 +2,11 @@
 
 data=$(varnishstat -j)
 
+jsn=$(echo $data | jq --arg INST "$INSTANCE" '. += { instance: $INST }')
+
 curl --request POST -L -k \
   --url http://varnish.atlas-ml.org:80/ \
   --header 'content-type: application/json' \
-  --data "$data"
+  --data "$jsn"
 
 echo "ok"
