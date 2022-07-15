88ffbaa44d0faa3ff25a4c8940c3e3657523aa3c
Ilija Vukotic <ivukotic@cern.ch>
2022-07-14 23:53:14 -0500

    checked. cleaned.

--- runme.sh
+++ runme.sh
@@ -1,4 +1,9 @@
+VARNISH_MEM=32G
+VARNISH_PORT=89
 echo "Starting Varnish on port $VARNISH_PORT with $VARNISH_MEM memory"
-service snmpd start
-su varnish -c '/usr/sbin/varnishd -F -f /etc/varnish/default.vcl -a http=:$VARNISH_PORT,HTTP -a proxy=:8443,PROXY -p feature=+http2 -s malloc,$VARNISH_MEM'
 
+# service snmpd start
+/usr/sbin/snmpd -LS 5 d -Lf /var/log/snmpd.log -p /var/run/snmpd.PID -a -d -V
+
+chsh -s /bin/bash varnish
+su varnish -c '/usr/sbin/varnishd -F -f /etc/varnish/default.vcl -a http=:$VARNISH_PORT,HTTP -a proxy=:8443,PROXY -p feature=+http2 -s malloc,$VARNISH_MEM'
