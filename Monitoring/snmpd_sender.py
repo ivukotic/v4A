4b19f224728eaad32f17753466b068fa5e9db3f0
Ilija Vukotic <ivukotic@cern.ch>
2022-07-18 18:53:28 -0500

    all important there. some with 0

--- Monitoring/snmpd_sender.py
+++ Monitoring/snmpd_sender.py
@@ -1,9 +1,31 @@
 #!/usr/bin/env python3
 import subprocess
+import os
 import sys
 
 
+def to_kB(vars):
+    inp = vars[0]
+    return int(int(inp)/1024)
+
+
+def to_MB(vars):
+    inp = vars[0]
+    return int(int(inp)/1048576)
+
+
+def get_instance(vars):
+    return os.getenv("SITE", default="TestSite")+os.getenv("INSTANCE", default="Inst-01")
+
+
 class VarnishStatus:
+
+    def get_beresp_kb(self, vars):
+        res = 0
+        for k in self.v_metrics.keys():
+            if k.startswith('VBE.boot.') and k.count('beresp'):
+                res += int(self.v_metrics[k])
+        return res/1024
 
     def __init__(self, oid_prefix):
 
@@ -14,92 +36,137 @@ class VarnishStatus:
         t = 'timeticks'
         o = 'octet'
 
-        self.v_s = {
-            "SMA.s0.c_bytes": ['1.1.1.0', i, 'Storage Mem size in KB'],  # TODO kB
-            "MAIN.uptime": ['1.1.3.0', t, 'The Uptime of the cache in timeticks'],
-            # TODO MB
-            "SMA.s0.g_space": ['1.2.5.1.0', i, 'The value of the cache_mem parameter in MB'],
-            "SMA.s0.c_bytes": ['1.3.1.3.0', i, 'cacheMemUsage'],  # TODO kB
-            'MAIN.n_object': ['1.3.1.7.0', i, 0, 'cacheNumObjCount'],
+        self.sq_oids = {
+            '1.1.1': [i, 9, 'cacheSysVMsize',  to_kB, "SMA.s0.c_bytes"],
+            '1.1.2': [i, 0, 'cacheSysStorage'],  # Storage Swap size in KB
+            '1.1.3': [t, 9, 'cacheUptime', "MAIN.uptime"],
+            '1.2.1': [s, 'ivukotic@uchicago.edu', 'cacheAdmin'],
+            '1.2.2': [s, 'varnish', 'cacheSoftware'],  # Cache Software Name
+            '1.2.3': [s, '7.1', 'cacheVersionId'],  # Cache Software Version
+            '1.2.4': [s, 'ALL,1', 'cacheLoggingFacility'],  # Logging Facility.
+            '1.2.5.1': [i, 9, 'cacheMemMaxSize', to_MB, "SMA.s0.g_space"],
+            '1.2.5.2': [i, 0, 'cacheSwapMaxSize'],
+            '1.2.5.3': [i, 0, 'cacheSwapHighWM'],
+            '1.2.5.4': [i, 0, 'cacheSwapLowWM'],
+            '1.2.6': [s, 'varnish-test', 'cacheUniqName', get_instance],
+            '1.3.1.1': [c, 0, 'cacheSysPageFaults'],  # ?
+            '1.3.1.2': [c, 0, 'cacheSysNumReads'],  # ?
+            '1.3.1.3': [i, 9, 'cacheMemUsage',  to_MB, "SMA.s0.c_bytes"],
+            '1.3.1.4': [i, 0, 'cacheCpuTime'],  # TODO accumulative CPU seconds
+            '1.3.1.5': [i, 0, 'cacheCpuUsage'],  # TODO percent usage
+            '1.3.1.6': [i, 0, 'cacheMaxResSize'],  # ?
+            '1.3.1.7': [g, 0, 'cacheNumObjCount', 'MAIN.n_object'],
+            '1.3.1.8': [t, 0, 'cacheCurrentLRUExpiration'],
+            '1.3.1.9': [g, 0, 'cacheCurrentUnlinkRequests'],
+            '1.3.1.10': [g, 0, 'cacheCurrentUnusedFDescrCnt'],
+            '1.3.1.11': [g, 0, 'cacheCurrentResFileDescrCnt'],
+            '1.3.1.12': [g, 0, 'cacheCurrentFileDescrCnt'],
+            '1.3.1.13': [g, 0, 'cacheCurrentFileDescrMax'],
+            '1.3.2.1.1': [c, 'cacheProtoClientHttpRequests', "MAIN.client_req"],
+            '1.3.2.1.2': [c, 'cacheHttpHits', "MAIN.cache_hit"],
+            '1.3.2.1.3': [c, 0, 'cacheHttpErrors'],  # ?
+            '1.3.2.1.4': [c, 0, 'cacheHttpInKb', self.get_beresp_kb],
+            '1.3.2.1.5': [c, 0, 'cacheHttpOutKb', to_kB, "MAIN.s_resp_bodybytes"],
+            '1.3.2.1.6': [c, 0, 'cacheIcpPktsSent'],
+            '1.3.2.1.7': [c, 0, 'cacheIcpPktsRecv'],
+            '1.3.2.1.8': [c, 0, 'cacheIcpKbSent'],
+            '1.3.2.1.9': [c, 0, 'cacheIcpKbRecv'],
+            '1.3.2.1.10': [i, 'cacheServerRequests', "MAIN.client_req"],
+            '1.3.2.1.11': [i, 0, 'cacheServerErrors'],
+            '1.3.2.1.12': [c, 0, 'cacheServerInKb'],  # ?
+            '1.3.2.1.13': [c, 0, 'cacheServerOutKb'],  # ?
+            '1.3.2.1.14': [g, 0, 'cacheCurrentSwapSize'],
+            '1.3.2.1.15': [g, 0, 'cacheClients'],  # ?
+            "1.3.2.2.1.1.1": [i, 1,  "cacheMedianTime.1"],  # ?
+            "1.3.2.2.1.1.5": [i, 5,  "cacheMedianTime.5"],  # ?
+            "1.3.2.2.1.1.60": [i, 60, "cacheMedianTime.60"],  # ?
+            "1.3.2.2.1.2.1": [i, 0,  "cacheHttpAllSvcTime.1"],
+            "1.3.2.2.1.2.5": [i, 0,  "cacheHttpAllSvcTime.5"],
+            "1.3.2.2.1.2.60": [i, 0, "cacheHttpAllSvcTime.60"],
+            "1.3.2.2.1.3.1": [i, 0,  "cacheHttpMissSvcTime.1"],  # TODO
+            "1.3.2.2.1.3.5": [i, 0,  "cacheHttpMissSvcTime.5"],  # TODO
+            "1.3.2.2.1.3.60": [i, 0, "cacheHttpMissSvcTime.60"],  # TODO
+            "1.3.2.2.1.4.1": [i, 0,  "cacheHttpNmSvcTime.1"],
+            "1.3.2.2.1.4.5": [i, 0,  "cacheHttpNmSvcTime.5"],
+            "1.3.2.2.1.4.60": [i, 0, "cacheHttpNmSvcTime.60"],
+            "1.3.2.2.1.5.1": [i, 0,  "cacheHttpHitSvcTime.1"],
+            "1.3.2.2.1.5.5": [i, 0,  "cacheHttpHitSvcTime.5"],
+            "1.3.2.2.1.5.60": [i, 0, "cacheHttpHitSvcTime.60"],
+            "1.3.2.2.1.6.1": [i, 0,  "cacheIcpQuerySvcTime.1"],
+            "1.3.2.2.1.6.5": [i, 0,  "cacheIcpQuerySvcTime.5"],
+            "1.3.2.2.1.6.60": [i, 0, "cacheIcpQuerySvcTime.60"],
+            "1.3.2.2.1.7.1": [i, 0,  "cacheIcpReplySvcTime.1"],
+            "1.3.2.2.1.7.5": [i, 0,  "cacheIcpReplySvcTime.5"],
+            "1.3.2.2.1.7.60": [i, 0, "cacheIcpReplySvcTime.60"],
+            "1.3.2.2.1.8.1": [i, 0,  "cacheDnsSvcTime.1"],
+            "1.3.2.2.1.8.5": [i, 0,  "cacheDnsSvcTime.5"],
+            "1.3.2.2.1.8.60": [i, 0, "cacheDnsSvcTime.60"],
+            "1.3.2.2.1.9.1": [i, 0,  "cacheRequestHitRatio.1"],  # TODO
+            "1.3.2.2.1.9.5": [i, 0,  "cacheRequestHitRatio.5"],  # TODO
+            "1.3.2.2.1.9.60": [i, 0, "cacheRequestHitRatio.60"],  # TODO
+            "1.3.2.2.1.10.1": [i, 0, "cacheRequestByteRatio.1"],  # TODO
+            "1.3.2.2.1.10.5": [i, 0, "cacheRequestByteRatio.5"],  # TODO
+            "1.3.2.2.1.10.60": [i, 0, "cacheRequestByteRatio.60"],  # TODO
+            "1.3.2.2.1.11.1": [i, 0, "cacheHttpNhSvcTime.1"],  # ?
+            "1.3.2.2.1.11.5": [i, 0, "cacheHttpNhSvcTime.5"],  # ?
+            "1.3.2.2.1.11.60": [i, 0, "cacheHttpNhSvcTime.60"],  # ?
+            "1.4.1.1": [g, 0,  "cacheIpEntries"],  # ?
+            "1.4.1.2": [c, 0,  "cacheIpRequests"],  # ?
+            "1.4.1.3": [c, 0,  "cacheIpHits"],  # ?
+            "1.4.1.4": [g, 0,  "cacheIpPendingHits"],
+            "1.4.1.5": [c, 0,  "cacheIpNegativeHits"],
+            "1.4.1.6": [c, 0,  "cacheIpMisses"],  # ?
+            "1.4.1.7": [c, 0,  "cacheBlockingGetHostByName"],
+            "1.4.1.8": [c, 0,  "cacheAttemptReleaseLckEntrries"],
+            "1.4.2.1": [g, 0,  "cacheFqdnEntries"],  # ?
+            "1.4.2.2": [c, 0,  "cacheFqdnRequests"],
+            "1.4.2.3": [c, 0,  "cacheFqdnHits"],
+            "1.4.2.4": [g, 0,  "cacheFqdnPendingHits"],
+            "1.4.2.5": [c, 0,  "cacheFqdnNegativeHits"],
+            "1.4.2.6": [c, 0,  "cacheFqdnMisses"],
+            "1.4.2.7": [c, 0,  "cacheBlockingGetHostByAddr"],
+            "1.4.3.1": [c, 0,  "cacheDnsRequests"],  # ?
+            "1.4.3.2": [c, 0,  "cacheDnsReplies"],  # ?
+            "1.4.3.3": [c, 0,  "cacheDnsNumberServers"]  # ?
         }
 
-        self.fakes = [
-            ['1.1.2.0', i, 0, 'Storage Swap size in KB'],
-            ['1.2.1.0', s, 'ivukotic@uchicago.edu', 'Cache Administrator E-Mail address'],
-            ['1.2.2.0', s, 'varnish', 'Cache Software Name'],
-            ['1.2.3.0', s, '7.1', 'Cache Software Version'],
-            ['1.2.4.0', s, 'ALL,1', 'Logging Facility.'],
-            ['1.2.5.2.0', i, 0, 'cacheSwapMaxSize'],
-            ['1.2.5.3.0', i, 0, 'cacheSwapHighWM'],
-            ['1.2.5.4.0', i, 0,  'cacheSwapLowWM'],
-            ['1.2.6.0', s, 'varnish-test', 'cacheUniqName'],  # TODO proper instance
-
-            ['1.3.1.1.0', i, 0, 'cacheSysPageFaults'],
-            ['1.3.1.2.0', i, 0, 'cacheSysNumReads'],
-            ['1.3.1.4.0', i, 0, 'cacheCpuTime'],
-            ['1.3.1.5.0', i, 0, 'cacheCpuUsage'],
-            ['1.3.1.6.0', i, 0, 'cacheMaxResSize'],
-            ['1.3.1.8.0', i, 0, 'cacheCurrentLRUExpiration'],
-            ['1.3.1.9.0', i, 0, 'cacheCurrentUnlinkRequests'],
-            ['1.3.1.10.0', i, 0, 'cacheCurrentUnusedFDescrCnt'],
-            ['1.3.1.11.0', i, 0, 'cacheCurrentResFileDescrCnt'],
-            ['1.3.1.12.0', i, 0, 'cacheCurrentFileDescrCnt'],
-            ['1.3.1.13.0', i, 0, 'cacheCurrentFileDescrMax'],
-        ]
-
         self.oid_prefix = oid_prefix
-
-        # self.data is a dictionary that contains all systemd service data using programmatically
-        # generated OID as the key:
-        # '1.3.9950.1.115.115.115.100': ('integer', 0, 'sssd'),
-        self.data = {}
-
-        # self.sorted_oids enables iterating over the OIDs in correct order
-        # while having the data in a non-ordered dictionary (self.data).
         self.sorted_oids = []
-
-        # Get systemd service status and store it in self.data. Also populate self.sorted_oids.
         self.cache_service_status()
 
-        # print('data', self.data)
         # print('sorted oids', self.sorted_oids)
+
+    def get_oid(self, oid):
+        for l in self.sorted_oids:
+            if l[0] == oid:
+                return l
 
     def cache_service_status(self):
 
         # Temporary data structure used to create sorted oid list later
-        oids = []  # self.oid_prefix]
+        self.sorted_oids = []
 
         lines = subprocess.check_output(["/usr/bin/varnishstat -1"],
                                         shell=True).decode("UTF-8").strip().split("\n")
 
         # print('metrics loaded:', len(lines))
+        self.v_metrics = {}
         for line in lines:
-            [v_name, v_val, v_smt] = line.split()[:3]
-            sq = self.get_sq(v_name)
-            if sq:
-                [sq_oid, sq_type, sq_name] = sq
-                sq_oid = self.oid_prefix+'.'+sq_oid
-                self.data[sq_oid] = (sq_type, v_val, sq_name)
-                oids.append(sq_oid)
+            [v_name, v_val] = line.split()[: 2]
+            self.v_metrics[v_name] = v_val
 
-        # print('add fakes')
-        for fake in self.fakes:
-            sq_oid = self.oid_prefix+'.'+fake[0]
-            oids.append(sq_oid)
-            self.data[sq_oid] = (fake[1], fake[2], fake[3])
-
-        # Sort the oids array. Lovingly borrowed from here:
-        #
-        # https://rosettacode.org/wiki/Sort_a_list_of_object_identifiers#Python
-        #
-        # The array sorted array is then used to output data in the correct order when using
-        # getnext (snmpwalk)
-        self.sorted_oids = sorted(oids, key=lambda x: list(map(int, x.split('.'))))
-
-    def get_sq(self, name):
-        if name in self.v_s:
-            return self.v_s[name]
-        return
+        for o, v in self.sq_oids.items():
+            fo = self.oid_prefix+'.'+o
+            if len(v) == 3:  # fixed value
+                self.sorted_oids.append([fo, v[0], v[1]])
+                continue
+            if isinstance(v[3], str):  # simple replacement
+                self.sorted_oids.append([fo, v[0], self.v_metrics[v[3]]])
+                continue
+            args = []
+            for c in v[4:]:
+                args.append(self.v_metrics[c])
+            self.sorted_oids.append([fo, v[0], v[3](args)])
 
 
 def getline():
@@ -140,40 +207,56 @@ def main():
 
             elif command == "get":
                 oid = getline()
+
+                # remove trailing zeroes from the oid
+                while len(oid) > 0 and oid[-2:] == ".0":
+                    oid = oid[: -2]
+
                 cl_oid = oid.lstrip(".")
-                if cl_oid in s.data:
-                    output(str(oid))
-                    output(str(s.data[cl_oid][0]))
-                    output(str(s.data[cl_oid][1]))
+
+                res = s.get_oid(cl_oid)
+                if res:
+                    output(res[0])
+                    output(res[1])
+                    output(str(res[1]))
                 else:
                     output("NONE")
 
             elif command == "getnext":
                 oid = getline()
-                cl_oid = oid.lstrip(".")
+                oid = oid.lstrip(".")
 
                 # remove trailing zeroes from the oid
-                while len(oid) > 0 and oid[-2:] == ".0" and oid not in s.sorted_oids:
-                    oid = oid[:-2]
+                while len(oid) > 0 and oid[-2:] == ".0":
+                    oid = oid[: -2]
 
-                # does provided oid exist in the list
-                if cl_oid in s.sorted_oids:
-                    # exact match return next one unless this was the last one
-                    ni = s.sorted_oids.index(cl_oid) + 1
+                # writelog('looking for oid:' + oid)
+                # does provided oid exist in the list?
+                found = False
+                ni = 0
+                for l in s.sorted_oids:
+                    ni += 1
+                    if oid == l[0]:
+                        # writelog("exact match, looking for the next")
+                        found = True  # exact match return next one unless this was the last one
+                        break
+                if found:
                     if ni >= len(s.sorted_oids) - 1:
                         output("NONE")
                     else:
-                        output("." + str(s.sorted_oids[ni]))
-                        output(str(s.data[s.sorted_oids[ni]][0]))
-                        output(str(s.data[s.sorted_oids[ni]][1]))
+                        output("." + str(s.sorted_oids[ni][0]))
+                        output(s.sorted_oids[ni][1])
+                        output(str(s.sorted_oids[ni][2]))
                 else:
-                    # not matched, try to find related
+                    # writelog('not matched, try to find related')
                     match = False
-                    for ex_oid in s.sorted_oids:
-                        if ex_oid.startswith(cl_oid):
-                            output("." + str(ex_oid))
-                            output(str(s.data[ex_oid][0]))
-                            output(str(s.data[ex_oid][1]))
+                    for ex in s.sorted_oids:
+                        ex_k = ex[0]
+                        ex_v = ex[1:]
+                        if ex_k.startswith(oid):
+                            output("." + str(ex_k))
+                            output(ex_v[0])
+                            output(str(ex_v[1]))
                             match = True
                             break
                     if not match:
--- Monitoring/test.sh
+++ Monitoring/test.sh
@@ -4,5 +4,7 @@ apt-get install snmpd snmp libsnmp-dev -y
 /usr/sbin/snmpd -LS 5 d -Lf /var/log/snmpd.log -p /var/run/snmpd.PID -a -d -V
 tail -f /var/log/snmpd.log
  
-snmpwalk  -v2c -Cc -c snmp_community localhost:3401 .1.3.6.1.4.1.3495.1.1
-snmpwalk  -v2c -Cc -c snmp_community localhost:3401 .1.3.6.1.4.1.3495
+snmpwalk  -v2c -Cc -c public localhost:3401 .1.3.6.1.4.1.3495.1.1.1.0
+snmpwalk  -v2c -Cc -c public localhost:3401 .1.3.6.1.4.1.3495.1.3.1.7.0
+snmpwalk  -v2c -Cc -c public localhost:3401 .1.3.6.1.4.1.3495.1.1
+snmpwalk  -v2c -Cc -c public localhost:3401 .1.3.6.1.4.1.3495
