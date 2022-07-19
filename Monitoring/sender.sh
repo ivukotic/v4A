c49d46f7932a4ace366d3a7992c3566c4848ec2a
Ilija Vukotic <ivukotic@cern.ch>
2022-07-19 17:15:56 -0500

    testing with stripped sender.sh

--- Monitoring/sender.sh
+++ Monitoring/sender.sh
@@ -2,12 +2,12 @@
 
 data=$(varnishstat -j)
 
-jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
+# jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
 
-curl --request POST -L -k 
-  --max-time 2 \
-  --url http://varnish.atlas-ml.org:80/ \
-  --header 'content-type: application/json' \
-  --data "$jsn"
+# curl --request POST -L -k 
+#   --max-time 2 \
+#   --url http://varnish.atlas-ml.org:80/ \
+#   --header 'content-type: application/json' \
+#   --data "$jsn"
 
 echo "ok"
