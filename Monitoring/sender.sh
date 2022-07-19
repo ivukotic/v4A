3e8681e47fd512e493c4448282c6611faa58f13a
Ilija Vukotic <ivukotic@cern.ch>
2022-07-19 18:31:11 -0500

    changed timeout

--- Monitoring/sender.sh
+++ Monitoring/sender.sh
@@ -1,13 +1,12 @@
 #!/bin/sh
 
-# data=$(varnishstat -j)
+data=$(varnishstat -j)
 
-# jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
+jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
 
-# curl --request POST -L -k 
-#   --max-time 2 \
-#   --url http://varnish.atlas-ml.org:80/ \
-#   --header 'content-type: application/json' \
-#   --data "$jsn"
+timeout 2 curl --request POST -L -k \
+  --url http://varnish.atlas-ml.org:80/ \
+  --header 'content-type: application/json' \
+  --data "$jsn"
 
 echo "ok"
