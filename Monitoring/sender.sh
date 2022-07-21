e5a1946ba5490681fffd202861dbb09d188ea691
Ilija Vukotic <ivukotic@cern.ch>
2022-07-21 11:40:10 -0500

    make monitoring optional

--- Monitoring/sender.sh
+++ Monitoring/sender.sh
@@ -1,5 +1,18 @@
 #!/bin/sh
 
+
+if [ -n $MONITOR_ES ]
+then
+  if [ $MONITOR_ES != "true" ]
+  then
+    echo "NO MONITOR OK"
+    exit 0
+  fi
+else
+  echo -e "MONITOR_ES not set.\n"
+  exit 0
+fi
+
 data=$(varnishstat -j)
 
 jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
@@ -9,4 +22,4 @@ timeout 2 curl --request POST -L -k \
   --header 'content-type: application/json' \
   --data "$jsn"
 
-echo "ok"
+exit 0
--- runme.sh
+++ runme.sh
@@ -1,7 +1,13 @@
 echo "Starting Varnish on port $VARNISH_PORT with $VARNISH_MEM memory"
 
-# service snmpd start
-/usr/sbin/snmpd -LS 5 d -Lf /var/log/snmpd.log -p /var/run/snmpd.PID -a -d -V
+if [ -n $MONITOR_SNMP ]
+then
+  if [ $MONITOR_SNMP == "true" ]
+  then
+    # service snmpd start
+    /usr/sbin/snmpd -LS 5 d -Lf /var/log/snmpd.log -p /var/run/snmpd.PID -a -d -V
+  fi
+fi
 
 chsh -s /bin/bash varnish
 su varnish -c '/usr/sbin/varnishd -F -f /etc/varnish/default.vcl -a http=:$VARNISH_PORT,HTTP -a proxy=:8443,PROXY -p feature=+http2 -s malloc,$VARNISH_MEM'
