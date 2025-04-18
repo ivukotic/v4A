ebe5ea969fc917d4aa273c40627913ba39844998
Ilija Vukotic <ivukotic@uchicago.edu>
2025-04-17 19:10:07 -0500

    Remove "MAIN*" from varnishstat command and update comments in sender.sh

--- Monitoring/sender.sh
+++ Monitoring/sender.sh
@@ -12,7 +12,9 @@ else
   exit 0
 fi
 
-data=$(varnishstat -j -X "VBE*" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA*" -X "MAIN*" -X "MGT*")
+data=$(varnishstat -j -X "VBE*" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA*" -X "MGT*")
+# the following line will simplify output but requires change in the logstash collector
+# fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
 fdata=$(echo $data | jq 'del(.counters[].description, .counters[].flag, .counters[].format)')
 jsn=$(echo $fdata | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
 
