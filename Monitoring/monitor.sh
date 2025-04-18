f2b23e81ecebeb12a920916444a4d0e6248158ae
Ilija Vukotic <ivukotic@uchicago.edu>
2025-04-18 12:20:23 -0500

    Refactor monitor.sh to filter varnishstat output and improve JSON formatting

--- Monitoring/monitor.sh
+++ Monitoring/monitor.sh
@@ -9,9 +9,11 @@ fi
 # infinite loop that repeats every 5 seconds
 while true; do
 
-    data=$(varnishstat -j)
-
-    jsn=$(echo $data | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
+    data=$(varnishstat -j -X "VBE*" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA*" -X "MGT*")
+    # the following line will simplify output but requires change in the logstash collector
+    # fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
+    fdata=$(echo $data | jq 'del(.counters[].description, .counters[].flag, .counters[].format)')
+    jsn=$(echo $fdata | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { instance: $INST, site: $SITE }')
 
     timeout 2 curl --request POST -L -k \
     --url http://varnish.atlas-ml.org:80/ \
