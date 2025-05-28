04b334095b43f1016efdf402029dbf55e1e48d95
Ilija Vukotic <ivukotic@uchicago.edu>
2025-05-27 19:30:14 -0500

    Enhance data collection in monitor.sh and sender.sh by adding connection count to JSON output and adjusting sleep duration

--- Monitoring/monitor.sh
+++ Monitoring/monitor.sh
@@ -9,18 +9,27 @@ fi
 # infinite loop that repeats every 5 seconds
 while true; do
 
-    data=$(varnishstat -j -X "VBE.boot.*.happy" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA*" -X "MGT*")
-    # the following line will simplify output but requires change in the logstash collector
-    # fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
-    fdata=$(echo $data | jq 'del(.counters[].description, .counters[].flag, .counters[].format)')
-    jsn=$(echo $fdata | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "cvmfs", instance: $INST, site: $SITE }')
+  data=$(varnishstat -j -X "VBE.boot.*.happy" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA*" -X "MGT*")
+  # the following line will simplify output but requires change in the logstash collector
+  # fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
+  fdata=$(echo $data | jq 'del(.counters[].description, .counters[].flag, .counters[].format)')
+  jsn=$(echo $fdata | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "cvmfs", instance: $INST, site: $SITE }')
 
-    timeout 2 curl --request POST -L -k \
-    --url http://varnish.atlas-ml.org:80/ \
+  timeout 2 curl --request POST -L -k \
+  --url http://varnish.atlas-ml.org:80/ \
+  --header 'content-type: application/json' \
+  --data "$jsn"
+
+  acc=$(ss -tpr | grep -v Address | awk '{print $5}' | awk -F: '{print $1}' | cut -f2- -d'.' | sort | uniq -c )
+  cnt=$(echo "$acc" | awk '{print "{ \"client\" : \"" $2 "\", \"connections\":" $1 "}"}' | jq -s '.')
+  ajs=$(echo "{\"kind\":\"cvmfs\",\"instance\":\"$INSTANCE\",\"site\":\"$SITE\"}" | jq | jq --argjson CNT "$cnt" '. +={ cnt: $CNT }')
+
+  timeout 2 curl --request POST -L -k \
+    --url 'http://varnish-accesses.atlas-ml.org:80/' \
     --header 'content-type: application/json' \
-    --data "$jsn"
+    --data "$ajs"
 
-    sleep 5
+  sleep 2
     
 done
 
--- Monitoring/sender.sh
+++ Monitoring/sender.sh
@@ -11,4 +11,13 @@ timeout 2 curl --request POST -L -k \
   --header 'content-type: application/json' \
   --data "$jsn"
 
+acc=$(ss -tpr | grep -v Address | awk '{print $5}' | awk -F: '{print $1}' | cut -f2- -d'.' | sort | uniq -c )
+cnt=$(echo "$acc" | awk '{print "{ \"client\" : \"" $2 "\", \"connections\":" $1 "}"}' | jq -s '.')
+ajs=$(echo "{\"kind\":\"cvmfs\",\"instance\":\"$INSTANCE\",\"site\":\"$SITE\"}" | jq | jq --argjson CNT "$cnt" '. +={ cnt: $CNT }')
+
+timeout 2 curl --request POST -L -k \
+  --url 'http://varnish-accesses.atlas-ml.org:80/' \
+  --header 'content-type: application/json' \
+  --data "$ajs"
+
 exit 0
