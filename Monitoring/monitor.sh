a1c84080f4e2071f324162314915a807e42cd681
Ilija Vukotic <ivukotic@uchicago.edu>
2025-06-29 14:26:06 +0300

    Refactor varnishstat data processing in monitor.sh and sender.sh for improved output handling

--- Monitoring/monitor.sh
+++ Monitoring/monitor.sh
@@ -10,10 +10,9 @@ fi
 while true; do
 
   data=$(varnishstat -j -X "VBE.boot.*.happy" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA.*c_*" -X "MGT*")
-  # the following line will simplify output but requires change in the logstash collector
-  # fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
-  fdata=$(echo $data | jq 'del(.counters[].description, .counters[].flag, .counters[].format)')
-  jsn=$(echo $fdata | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "cvmfs", instance: $INST, site: $SITE }')
+  fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
+  fdatb=$(echo $fdata | jq '.counters as $c | del(.counters) + $c')
+  jsn=$(echo $fdatb | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "cvmfs", instance: $INST, site: $SITE }')
 
   timeout 2 curl --request POST -L -k \
   --url http://varnish.atlas-ml.org:80/ \
