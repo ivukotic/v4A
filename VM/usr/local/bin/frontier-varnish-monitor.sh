d222ae4583ab369453dd58365185b26e27409f10
Ilija Vukotic <ivukotic@uchicago.edu>
2025-07-04 08:35:51 +0300

    Add configuration files for Varnish monitoring and service setup

---
+++ VM/usr/local/bin/frontier-varnish-monitor.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# check variables $INSTANCE and $SITE are defined
+if [ -z "$INSTANCE" ] || [ -z "$SITE" ]; then
+  echo "Error: INSTANCE and SITE variables must be set."
+  exit 1
+fi
+
+# infinite loop that repeats every 5 seconds
+while true; do
+
+  data=$(varnishstat -j -X "VBE.boot.*.happy" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA.*c_*" -X "MGT*")
+  fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
+  fdatb=$(echo $fdata | jq '.counters as $c | del(.counters) + $c')
+  jsn=$(echo $fdatb | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "frontier", instance: $INST, site: $SITE }')
+
+  timeout 2 curl --request POST -s -q -L -k -o /dev/null \
+  --url "$URL_ML" \
+  --header 'content-type: application/json' \
+  --data "$jsn"
+
+
+  acc=$(ss -tpH | awk '{print $5}' | awk -F ':' '{ if ($1 ~ /^\[/) { n = NF - 1;  s = $1; for (i = 2; i <= n; i++) s = s ":" $i; print s } else { print $1 } }' | sort | uniq -c )
+  cnt=$(echo "$acc" | awk '{print "{ \"ip\" : \"" $2 "\", \"connections\":" $1 "}"}' | jq -s '.')
+  ajs=$(echo "{\"kind\":\"frontier\",\"instance\":\"$INSTANCE\",\"site\":\"$SITE\"}" | jq | jq --argjson CNT "$cnt" '. +={ cnt: $CNT }')
+
+  timeout 2 curl --request POST -s -q -L -k -o /dev/null \
+    --url "$URL_ML_ACCESS" \
+    --header 'content-type: application/json' \
+    --data "$ajs"
+
+  sleep 2
+    
+done
+
