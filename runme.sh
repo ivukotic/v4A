f18021cea4e408f2894ec2acdd261389acc5203c
Ilija Vukotic <ivukotic@uchicago.edu>
2025-08-26 16:50:15 -0500

    Remove unused neo_frontier_streaming.vcl configuration and update runme.sh to add nuke_limit parameter for Varnish

--- runme.sh
+++ runme.sh
@@ -39,4 +39,4 @@ source /usr/local/bin/reconfiguration.sh $version &
 echo "Starting Varnish on port $VARNISH_PORT"
 echo "Using $VARNISH_MEM memory, and $VARNISH_TRANSIENT_MEM and config file $nfile.vcl version $version"
 
-/usr/sbin/varnishd -F -f /tmp/$nfile.vcl -a http=:$VARNISH_PORT,HTTP -a proxy=:8443,PROXY -p feature=+http2 -p max_restarts=8 -s malloc,$VARNISH_MEM -s Transient=malloc,$VARNISH_TRANSIENT_MEM
+/usr/sbin/varnishd -F -f /tmp/$nfile.vcl -a http=:$VARNISH_PORT,HTTP -a proxy=:8443,PROXY -p feature=+http2 -p max_restarts=8 -p nuke_limit=1000 -s malloc,$VARNISH_MEM -s Transient=malloc,$VARNISH_TRANSIENT_MEM
