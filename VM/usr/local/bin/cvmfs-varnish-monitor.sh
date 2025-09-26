454976c218e96ff2007e6738539912829cdeeaf8
Ilija Vukotic <ivukotic@uchicago.edu>
2025-09-26 05:49:00 +0200

    Add Varnish configuration and monitoring scripts for CVMFS

---
+++ VM/usr/local/bin/cvmfs-varnish-monitor.sh
@@ -0,0 +1,35 @@
+#!/bin/sh
+
+# check variables $INSTANCE and $SITE are defined
+if [ -z "$INSTANCE" ] || [ -z "$SITE" ]; then
+  echo "Error: INSTANCE and SITE variables must be set."
+  exit 1
+fi
+
+# infinite loop that repeats every 5 seconds
+while true; do
+
+  data=$(varnishstat -j -X "VBE.*.happy" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA.*c_*" -X "MGT*")
+  fdata=$(echo $data | jq '.counters |= with_entries(.value = .value["value"])')
+  fdatb=$(echo $fdata | jq '.counters as $c | del(.counters) + $c')
+  jsn=$(echo $fdatb | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "cvmfs", instance: $INST, site: $SITE }')
+
+  timeout 2 curl --request POST -L -k \
+  --url http://varnish.atlas-ml.org:80/ \
+  --header 'content-type: application/json' \
+  --data "$jsn"
+
+
+  acc=$(ss -tpH | awk '{print $5}' | awk -F: '{print $1}' | sort | uniq -c )
+  cnt=$(echo "$acc" | awk '{print "{ \"ip\" : \"" $2 "\", \"connections\":" $1 "}"}' | jq -s '.')
+  ajs=$(echo "{\"kind\":\"cvmfs\",\"instance\":\"$INSTANCE\",\"site\":\"$SITE\"}" | jq | jq --argjson CNT "$cnt" '. +={ cnt: $CNT }')
+
+  timeout 2 curl --request POST -L -k \
+    --url 'http://varnish-accesses.atlas-ml.org:80/' \
+    --header 'content-type: application/json' \
+    --data "$ajs"
+
+  sleep 2
+    
+done
+
