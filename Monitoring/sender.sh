10f7270fb960d6b7f5dc76dabe8f68e052ebbe1d
Ilija Vukotic <ivukotic@uchicago.edu>
2025-11-21 14:55:45 -0600

    Update configurations and scripts for frontier node setup, including version updates and improved configuration handling

--- Monitoring/sender.sh
+++ Monitoring/sender.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
-
+ver="v2025-11-20"
 data=$(varnishstat -j -X "VBE.*.happy" -X "WAITER*" -X "LCK*" -X "MEM*" -X "SMA.*c_*" -X "MGT*")
 fdata=$(echo "$data" | jq '.counters |= with_entries(.value = .value["value"])')
 fdatb=$(echo "$fdata" | jq '.counters as $c | del(.counters) + $c')
-jsn=$(echo "$fdatb" | jq --arg INST "$INSTANCE" --arg SITE "$SITE" '. += { kind: "frontier", instance: $INST, site: $SITE }')
+jsn=$(echo "$fdatb" | jq --arg INST "$INSTANCE" --arg SITE "$SITE" --arg VER "$ver" '. += { kind: "frontier", instance: $INST, site: $SITE, ver: $VER }')
 
 timeout 2 curl --request POST -s -q -L -k \
   --url 'http://varnish.atlas-ml.org:80/' \
--- reconfiguration.sh
+++ reconfiguration.sh
@@ -17,7 +17,7 @@ while true; do
     if [ "$current_minute" -eq "$x" ]; then
         # echo "Downloading file at minute $x..."
         while true; do
-            ma=$(curl -s "https://raw.githubusercontent.com/ivukotic/v4A/frontier/configurations/mapping.json")
+            ma=$(curl -s "https://raw.githubusercontent.com/ivukotic/v4A/frontier/configurations/configurations.json")
             
             # Check if curl was successful
             if [ $? -eq 0 ] && [ -n "$ma" ]; then
@@ -29,19 +29,23 @@ while true; do
             fi
         done
 
-        # get value of SITE.INSTANCE 
-        config=$(echo "$ma" | jq -r --arg site "$SITE" --arg instance "$INSTANCE" '.[$site][$instance]')
+        config=$(echo "$ma" | jq -r --arg site $SITE --arg instance $INSTANCE \
+        '  . as $root
+        | (
+            $root.sites
+            | map(select(.name == $site))
+            | .[0] as $siteObj
+            | if $siteObj then
+                ($siteObj.instances // [] | map(select(.name == $instance)) | .[0]) as $instObj
+                | ($instObj.file // $siteObj.file // $root.file)
+                else
+                $root.file
+                end
+            )
+        ' )
 
-        # Check if the value exists
-        if [ -z "$config" ] || [ "$config" == null ]; then
-            echo "No value found for $SITE.$INSTANCE, using default value"
-            config=$(echo "$ma" | jq -r '.default')
-            echo "Default value: $config"
-            nfile=$(echo "$config" | jq -r '.file')
-        else
-            echo "Value of $SITE.$INSTANCE: $config"
-            nfile=$(echo "$config" | jq -r '.file')
-        fi
+        echo "Value of $SITE.$INSTANCE: $config"
+        nfile=$(echo "$config" | jq -r '.file')
 
         if [ "$current_version" == "$nfile" ]; then
             # echo "Skipping this loop iteration as $current_version matches $nfile..."
@@ -59,7 +63,6 @@ while true; do
 
             varnishadm vcl.load $nfile /tmp/$nfile.vcl && varnishadm vcl.use $nfile && current_version="$nfile"
         fi
-
     fi
 
     # Sleep for 1 minute
