2d7dad4059007bd1fb3c62189a1eb2824ffb7230
Ilija Vukotic <ivukotic@uchicago.edu>
2026-03-06 14:47:39 -0600

    Update configurations for SWT2_CPB; add URLs and modify instances

--- Monitoring/tester.py
+++ Monitoring/tester.py
@@ -13,10 +13,12 @@ if not es_user or not es_password:
     print("to store data ES_USER and ES_PASSWORD environment variables must be set.")
     no_ES = True
 
+
 def add_missing_defaults(parent, child):
     for key in ['port', 'type', 'file', 'active', 'local', 'in_CDN', 'responsible']:
         if key not in child:
             child[key] = parent.get(key)
+
 
 def denormalize_configurations(d):
     for site in d.get('sites', []):
@@ -25,19 +27,25 @@ def denormalize_configurations(d):
             add_missing_defaults(site, instance)
     return d
 
+
 def load_configurations():
     resp = requests.get(URL, timeout=15)
     resp.raise_for_status()          # fail fast if the download didn’t work
     data = json.loads(resp.text)     # or: resp.json()
     if not isinstance(data, object):
-        raise ValueError("Expected a top-level JSON object, got something else!")
+        raise ValueError(
+            "Expected a top-level JSON object, got something else!")
     return denormalize_configurations(data)
 
 # function that returns an elasticsearch client
 # login credentials are read from the environment variables ES_USER and ES_PASSWORD
+
+
 def get_es_client():
-    es = Elasticsearch("https://atlas-kibana.mwt2.org:9200", basic_auth=(es_user, es_password))
+    es = Elasticsearch("https://atlas-kibana.mwt2.org:9200",
+                       basic_auth=(es_user, es_password))
     return es
+
 
 headers = {
     "X-frontier-id": "varnish-tester",
@@ -45,9 +53,12 @@ headers = {
 }
 
 # function to test individual endpoints
+
+
 def test_endpoint(site) -> bool:
     try:
-        response = requests.get(f'http://{site["url"]}:{site["port"]}/atlr', timeout=10, headers=headers)
+        response = requests.get(
+            f'http://{site["url"]}:{site["port"]}/atlr', timeout=10, headers=headers)
         return response.status_code
     except requests.RequestException as e:
         print(f"Error testing endpoint {site['url']}: {e}")
@@ -56,7 +67,7 @@ def test_endpoint(site) -> bool:
 
 if __name__ == "__main__":
     cs = load_configurations()
-    
+
     if not no_ES:
         es = get_es_client()
     print(f"Loaded {len(cs['sites'])} endpoint definitions")
@@ -69,7 +80,7 @@ if __name__ == "__main__":
             continue
         if site['active'] and not site['local']:
             status = test_endpoint(site)
-            document={
+            document = {
                 "address": site['url'],
                 "status": status,
                 "@timestamp": datetime.now(timezone.utc),
@@ -77,7 +88,7 @@ if __name__ == "__main__":
                 "kind": "conditions"
             }
             print(document)
-            
+
             try:
                 if no_ES:
                     continue
